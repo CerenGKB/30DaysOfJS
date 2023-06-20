36ba23f689741971b85bd47b6f04dee93ea75874
CerenGKB <cerengokbulut@hotmail.com>
2023-06-20 14:18:06 +0300

    Day 1 Variables

---
+++ Day 1/Day1.js
@@ -0,0 +1,25 @@
+
+//Welcome to 30 Days of JavaScript
+
+
+//variables
+
+let text = "i am a string"
+let anAge = 23
+let weirdNum = 3.5
+let truthValue = true
+
+let myAge = 25
+let yourAge = 30
+
+
+console.log(typeof text)
+console.log(typeof anAge)
+console.log(typeof weirdNum)
+console.log(typeof truthValue)
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
+
+
+console.log("I am " + myAge + " years old")
+console.log("You are " + yourAge + " years old")
