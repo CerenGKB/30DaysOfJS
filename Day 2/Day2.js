aa50c51afab1d663dcd72c452e838049218e395b
CerenGKB <cerengokbulut@hotmail.com>
2023-06-21 23:39:18 +0300

    Some Challenges for Day 2

---
+++ Day 2/Day2.js
@@ -0,0 +1,20 @@
+let string = '30 days of javascript'
+
+let company = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+
+console.log(string)
+
+
+console.log('1-' + string.length)
+
+console.log('2-' + string.toUpperCase())
+
+console.log('3-' + string.toLowerCase())
+
+console.log('4-' + string.substring(3,7))
+
+console.log('5-' + string.split(' '))
+
+console.log('6-' + company.split(','))
+
+console.log('7-' + string.replace('javascript', "Python"))
