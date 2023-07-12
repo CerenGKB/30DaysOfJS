ed67d4d77795e8227d2930e15916eb7228f2b176
CerenGKB <cerengokbulut@hotmail.com>
2023-07-12 13:37:48 +0300

    final updates of day1

--- Day 1/Day1.js
+++ Day 1/Day1.js
@@ -2,25 +2,25 @@
 
 //Comments make code more readable
 
-
 //variables
 
 let text = "i am a string"
 let anAge = 23
 let weirdNum = 3.5
 let truthValue = true
+let empytValue 
+let emptyObj = null
 
 let myAge = 25
 let yourAge = 30
 
-
-console.log(typeof text)
-console.log(typeof anAge)
-console.log(typeof weirdNum)
-console.log(typeof truthValue)
+console.log(typeof text) //String
+console.log(typeof anAge) //number
+console.log(typeof weirdNum) // number
+console.log(typeof truthValue) // boolean
 console.log(typeof null) //object
-console.log(typeof undefined) // undefined
-
+console.log(typeof empytValue) // undefined
+console.log(typeof emptyObj) // object
 
 console.log("I am " + myAge + " years old")
 console.log("You are " + yourAge + " years old")
