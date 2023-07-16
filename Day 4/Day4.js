98714ba55569dd6e647729c10a5582bbc0300b57
CerenGKB <cerengokbulut@hotmail.com>
2023-07-16 19:45:29 +0300

    Day 3 exercises conditionals

---
+++ Day 4/Day4.js
@@ -0,0 +1,124 @@
+
+// Exercise level 1 - 1 
+
+/* let usersAge = prompt("Enter Your Age :")
+
+if(usersAge>=18){
+    console.log('You are old enough to drive')
+} else {
+    let necessaryYears = 18 - usersAge
+    console.log(`You have ${necessaryYears} years to drive`)
+} */
+
+
+
+// Exercise level 1 - 2
+
+/* let a = prompt("Enter A:")
+let b = prompt("Enter B:")
+
+if(a>b){
+    console.log(`${a} is greater than ${b}`)
+} else if(a <b){
+    console.log(`${a} is smaller than ${b}`)
+} else{
+    console.log(`${a} equals to ${b}`)
+} */
+
+
+// Exercise level 1 - 3
+
+/* let usersNumber = prompt("Enter a Number: ")
+
+if(usersNumber%2 === 0) {
+    console.log(usersNumber + " is an even number")
+}else {
+    console.log(usersNumber + ' is an odd number')
+} */
+
+
+//Exercise Level 2 : 1-6 
+
+/* 
+let grade = prompt('Enter your grade: ')
+
+if(grade > 89){
+    console.log('Your grade is A')
+}else if(grade > 69){
+    console.log('Your grade is B')
+}else if( grade > 59){
+    console.log('Your grade is C')
+}else if(grade > 49){
+    console.log('Your grade is D')
+}else {
+    console.log('Your grade is F')
+} */
+
+
+//Exercises Level 2 : 7-11
+
+
+/* let usersMonth = prompt("Enter the month: ")
+let month = usersMonth.toLowerCase()
+
+switch(month){
+    case 'september':
+    case 'october':
+    case 'november':
+        console.log('The season is Autumn')
+        break
+    case 'december':
+    case 'january':
+    case 'february':
+        console.log('The season is Winter')
+        break
+    case 'march':
+    case 'april':
+    case 'may':
+        console.log('The season is Spring')
+        break
+    case 'june':
+    case 'july':
+    case 'august':
+        console.log('The season is Summer')
+        break
+    
+    }
+
+
+ */
+
+
+
+//Exercises level 2 - 12
+
+/* 
+let dayUserInput = prompt('What day is today?: ')
+let day = dayUserInput.toLowerCase()
+
+
+switch(day){
+case 'monday':
+    console.log('Monday is a working day')
+    break
+case 'tuesday':
+    console.log('Tuesday is a working day')
+    break   
+case 'wednesday':
+    console.log('Wednesday is a working day')
+    break
+case 'thursday':
+    console.log('Thursday is a working day')
+    break
+case 'friday':
+    console.log('Friday is a working day')
+    break
+case 'saturday':
+    console.log('Saturday is a working day')
+    break
+case 'sunday':
+    console.log('Sunday is a working day')
+    break
+
+} */
+
