565461e3a0423e42871fdf08082ec8817d41c1e8
CerenGKB <cerengokbulut@hotmail.com>
2023-07-20 23:14:50 +0300

    Started Array Exercises

---
+++ Day 5/Day5.js
@@ -0,0 +1,31 @@
+
+
+// 1 - create an empty array
+const newArr = []
+
+// 2- create an array with more than 5 numbers of el
+const newArrNums = [1,2,3,4,5,6]
+
+// 3- Find the length of your array
+console.log("Length of array is " + (newArrNums.length))
+
+//4- Get the first item, the middle item and the last item of the array
+console.log('First element is ' + newArrNums[0]) //first index
+let middleEl = Math.floor(newArrNums.length / 2)
+console.log('Middle element is ' + middleEl)
+let lastEl = newArrNums.length - 1
+console.log('Last Element is ' + lastEl)
+
+
+// 5 -Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+const mixedDataTypes = [1,2,3,"ice cream" , 'pen' ,'kebab']
+
+// 6 - Declare an array variable itCompanies somerandom and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+const itCompanies = ['Facebook', 'Google' ,'Microsoft' ,'Apple', "IBM","Oracle", "Amazon"]
+
+// 7- Print the array using console.log()
+console.log("Company array is " + itCompanies)
+
+for(const element of newArrNums){
+    console.log(element)
+}
