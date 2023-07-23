dc5e8870a3a038e2c4c66e7ca3fe186bfb5393b5
CerenGKB <cerengokbulut@hotmail.com>
2023-07-23 16:40:16 +0300

    Finished all exercises in level 1

--- Day 5/Day5.js
+++ Day 5/Day5.js
@@ -43,3 +43,79 @@ console.log(...itCompanies)
 // 11 -Change each company name to uppercase one by one and print them out
 const upperCaseArr = itCompanies.map(element => element.toUpperCase())
 console.log('Array with uppercase is: ' + upperCaseArr)
+
+// 12 Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+
+
+const lastCompanyInArr = itCompanies[itCompanies.length - 1];
+const otherCompanies = itCompanies.slice(0, itCompanies.length - 1).join(', ')
+
+console.log(`${otherCompanies} and ${lastCompanyInArr} are big IT companies.`)
+
+// 13 Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+
+function findCompany(companyName) {
+    if (itCompanies.includes(companyName)) {
+        return `${companyName} exists in our data`
+    } else {
+        return `${companyName} is not found.`;
+    }
+}
+console.log(findCompany('Facebook'))
+console.log(findCompany('Starbucks'))
+
+
+// 14 - Filter out companies which have more than one 'o' without the filter method
+
+const regex = /.*[oO].*[oO].*/;
+
+let result = itCompanies.filter(company => regex.test(company));
+
+console.log("Companies with at least 2 \"O\" letter in them are " + result);
+
+// 15 - Sort the array using sort() method
+
+console.log("Sorted array ıs " + itCompanies.sort())
+
+// 16 - Reverse the array using reverse() method
+
+console.log("Reversed array is " + itCompanies.reverse())
+
+// 17 - Slice out the first 3 companies from the array
+
+console.log("Array without first 3 items are " + itCompanies.slice(3))
+
+// 18 - Slice out the last 3 companies from the array
+
+console.log("Array without last 3 items are " + itCompanies.splice(0,itCompanies.length - 3))
+
+// 19 - Slice out the middle IT company or companies from the array
+
+const companyInMiddle = Math.floor(itCompanies.length / 2 )
+
+console.log("Company in the middle is " + itCompanies[companyInMiddle])
+
+// 20 - Remove the first IT company from the array
+
+const  newItCompanies = ['Facebook', 'Google' ,'Microsoft' ,'Apple', "IBM","Oracle", "Amazon"]
+/* 
+newItCompanies.shift()
+console.log("Array without first item is " + newItCompanies )
+ */
+
+
+// 21 - Remove the middle IT company from array
+
+const companyMiddle = Math.floor(newItCompanies.length / 2 )
+
+console.log(newItCompanies.splice(companyMiddle, 1))
+
+// 22 - Remove the last IT company from the array
+
+let lastCompanyInArray = newItCompanies.pop();
+
+console.log("Array without last item is " + newItCompanies)
+
+// 23 - Remove all
+
+console.log(newItCompanies.splice(0,newItCompanies.length))
