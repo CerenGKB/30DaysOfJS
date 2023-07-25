2f2f3e556ce2fad69fd4bb7ce3f7cfdaf1e4b0ae
CerenGKB <cerengokbulut@hotmail.com>
2023-07-25 22:54:36 +0300

    Added 2 more exercises on loops

--- Day 6/Day6.js
+++ Day 6/Day6.js
@@ -27,6 +27,37 @@ const countries = [
 
   // 1 -Iterate 0 to 10 using for loop, do the same using while and do while loop
 
+  let zeroToTen = ''
   for(let i=0 ; i<=10 ; i++){
-    console.log(i)
+    zeroToTen += i + ' '
+  }
+  console.log(zeroToTen)
+
+  // 2 - Iterate 10 to 0 using for loop, do the same using while and do while loop
+
+  let tenToZero = ''
+  for(let i=10 ; i>=0 ; i--){
+    tenToZero += i + ' '
+  }
+  console.log(tenToZero)
+
+  // 3 - Iterate 0 to n using for loop
+
+  let zeroToN = ''
+  let n = 20 
+
+  for (let i = 0; i <= n; i++) {
+     zeroToN += i + ' '
+  }
+
+  console.log(zeroToN) 
+
+  // 4 - Write a loop that makes the following pattern using console.log()
+
+  let a = 7
+  let pattern = ''
+  
+  for (let i = 0; i < a; i++) {
+      pattern += '#'
+      console.log(pattern)
   }
